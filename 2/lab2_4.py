e91cded575c1e6ea439cc268fda47fc4142042b1
Jakub <Kubolakuba98@gmail.com>
2019-03-20 19:56:09 +0100

    L2

---
+++ 2/lab2_4.py
@@ -0,0 +1,11 @@
+#4 Add rounding for the above x/y operation. Round to 2 decimal points. Hint: look up in Google "python limiting number of decimals". (1p)
+
+from cs50 import *
+
+x = get_float("Input X: ")
+y = get_float("Input Y: ")
+
+while y == 0:
+    y = get_float("Y = 0, input new Y: ")
+
+print(round(x/y, 2))
