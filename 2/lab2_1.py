2e618acb2a6d42c0b0666c3462ae819135fced07
Jakub <Kubolakuba98@gmail.com>
2019-03-21 14:10:52 +0100

    L2

--- 2/lab2_1.py
+++ 2/lab2_1.py
@@ -1,7 +1,7 @@
 #1 Perimeter & field of circles with given radius X for the first circle & Y for the second one. (1p)
 
 
-from cs50 import *
+from cs50 import get_float
 from math import pi
 x = get_float("First circle radius: ")
 y = get_float("Second circle radius: ")
@@ -12,7 +12,7 @@ while x <= 0:
 while y <= 0:
     y = get_float("Radius y is smaller than 0, type another one: ")
 
-print("Perimeter x: ", 4*3.14*x)
-print("Perimeter y: ", 4 * 3.14 * y)
+print("Perimeter x: ", 2 * 3.14 * x)
+print("Perimeter y: ", 2 * 3.14 * y)
 print("Field x: ", 3.14 * x ** 2)
 print("Field y: ", 3.14 * y ** 2)
